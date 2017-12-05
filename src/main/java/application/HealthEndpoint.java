91251e05d31e9d56866850860ab047ba090d26b5
David Currie <david_currie@uk.ibm.com>
2017-12-05 15:50:27 +0000

    Use MicroProfile HealthCheck

--- src/main/java/application/HealthEndpoint.java
+++ src/main/java/application/HealthEndpoint.java
@@ -1,24 +1,18 @@
-package application.rest;
+package application;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Application;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import javax.enterprise.context.ApplicationScoped;
 
-@Path("health")
-public class HealthEndpoint {
+import org.eclipse.microprofile.health.Health;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response healthcheck() {
-      /*
-      if (!healthy) {
-        return Response.status(503).entity("{\"status\":\"DOWN\"}").build();
-      }
-      */
-      return Response.ok("{\"status\":\"UP\"}").build();
+@Health
+@ApplicationScoped
+public class HealthEndpoint implements HealthCheck {
+
+    @Override
+    public HealthCheckResponse call() {
+      return HealthCheckResponse.named("demo").up().build();
     }
 
 }

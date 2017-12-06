6e8ab1e42f77a53b3c4c842505d3806c2141462c
David Currie <david_currie@uk.ibm.com>
2017-12-06 14:36:03 +0000

    Updates

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM websphere-liberty:beta
-COPY src/main/liberty/config /config/
-COPY target/demo-1.0-SNAPSHOT.war /config/dropins/
 RUN wget http://central.maven.org/maven2/net/wasdev/wlp/tracer/liberty-opentracing-zipkintracer/1.0/liberty-opentracing-zipkintracer-1.0-sample.zip -O /tmp/tracer.zip && \
   unzip /tmp/tracer.zip -d /opt/ibm/wlp/usr && \
   rm /tmp/tracer.zip
+COPY src/main/liberty/config /config/
+COPY target/demo-1.0-SNAPSHOT.war /config/dropins/
 
--- src/main/java/application/rest/v1/Example.java
+++ src/main/java/application/rest/v1/Example.java
@@ -1,24 +1,22 @@
 package application.rest.v1;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("v1/example")
 public class Example {
 
+    @Inject
+    private BackendCaller caller;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public Response example() {
-        Client client = ClientBuilder.newClient();
-        Response response = client.target("http://demo-backend-service:9080/v1/example").request().get();
-        String message = response.readEntity(String.class);
-        client.close();
-        return Response.ok("Received: " + message).build();
+        return Response.ok("Received: " + caller.call()).build();
     }
 
 }
